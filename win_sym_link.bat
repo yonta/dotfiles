4c0a37e4f0fbf904e2c0a3c7bbdfa13f5110d722
SAITOU Keita <keita44.f4@gmail.com>
2016-10-25 23:48:01 +0900

    Update: git環境に合わせて各種設定を変更
    rebaseコマンドを削除
    レポジトリパスを更新
    ignoreファイルを変更

--- win_sym_link.bat
+++ win_sym_link.bat
@@ -5,11 +5,11 @@ cd /d %~dp0
 set current=%CD%
 
 set home=e:\home
-set hgpath=%home%\hg\config
+set repopath=%current%
 
-mklink %home%\.profile %hgpath%\bash\.profile
-mklink %home%\.gitconfig %hgpath%\git\.gitconfig
-mklink %home%\.gitignore_global %hgpath%\git\.gitignore_global
-mklink %home%\.hgrc %hgpath%\mercurial\.hgrc
-mklink %home%\.hgignore_global %hgpath%\mercurial\.hgignore_global
-mklink /D %home%\.emacs.d %hgpath%\emacs
+mklink %home%\.profile %repopath%\bash\.profile
+mklink %home%\.gitconfig %repopath%\git\.gitconfig
+mklink %home%\.gitignore_global %repopath%\git\.gitignore_global
+mklink %home%\.hgrc %repopath%\mercurial\.hgrc
+mklink %home%\.hgignore_global %repopath%\mercurial\.hgignore_global
+mklink /D %home%\.emacs.d %repopath%\emacs
