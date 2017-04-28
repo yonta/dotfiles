9631a920f9bb5ee0fcbd951968af5eea02bf75c2
SAITOU Keita <keita44.f4@gmail.com>
2017-04-28 18:38:55 +0900

    Fix: company-modeの設定ミスを修正

--- emacs/lisp/init_mode.el
+++ emacs/lisp/init_mode.el
@@ -172,7 +172,7 @@ Creates a buffer if necessary."
   (global-set-key (kbd "C-M-i") 'company-complete)
   ;; (setq company-idle-delay nil) ; 自動補完をしない
   (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
   (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
   )
