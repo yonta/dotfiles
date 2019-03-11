576a0e87527a886b8eae6c1799830996ba0fad86
SAITOU Keita <keita44.f4@gmail.com>
2019-03-11 19:34:26 +0900

    Remove: Emacsでバックアップはとらない、今後はバックアップが必要ならgitに任せる

--- .emacs.d/lisp/init_behavior.el
+++ .emacs.d/lisp/init_behavior.el
@@ -12,16 +12,8 @@
 ;;; 問い合わせを簡略化 yes/no を y/n
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;;; バックアップファイルを"~/.emacs.d/backup"に集める
-; backupフォルダの作成
-(defvar my-backup-dir "~/.emacs.d/backup")
-(unless (file-directory-p my-backup-dir) (make-directory my-backup-dir t))
-; バックアップファイルの保存先の指定
-(setq backup-directory-alist '((".*" . "~/.emacs.d/backup")))
-
-;;; バージョンコントロールでバックアップファイルを作らないようにする
-;;; 現在はなぜか効果がでない
-(setq version-control nil)
+;;; バックアップファイルを作らない
+(setq make-backup-files nil)
 
 ;;; 画像ファイルを表示する
 (auto-image-file-mode t)
