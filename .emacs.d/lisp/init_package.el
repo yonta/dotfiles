62c738ec465cc71ca79be34675b5c26cae0ba007
SAITOU Keita <keita44.f4@gmail.com>
2019-03-13 21:33:13 +0900

    Remove: Delphyはもうかかない
    ついでに、パッケージリストの並びを変更

--- .emacs.d/lisp/init_package.el
+++ .emacs.d/lisp/init_package.el
@@ -3,9 +3,9 @@
 (defvar my-package-list
   '(
     ; なぜか1つ目のパッケージがエラー、`packagename-`とハイフンがついた
-    ; パッケージが無いと怒られる、手動で入れるとOK
+    ; パッケージが無いと怒られる。
     ; 初回起動はlist-packageの更新が間に合ってない？
-    ; 一度list-packageを実行すると治る
+    ; 一度list-packageを実行すると治る。
     ;; ocaml
     flycheck-ocaml tuareg yasnippet
     ;; arduino
@@ -19,12 +19,26 @@
     ;; コードブロックの編集のため、edit-indirectが必要
     markdown-mode edit-indirect
     ;; other mode
-    csv-mode sml-mode twittering-mode gnuplot-mode graphviz-dot-mode
-    google-translate bash-completion haxe-mode proof-general
+    csv-mode
+    sml-mode
+    twittering-mode
+    gnuplot-mode
+    graphviz-dot-mode
+    google-translate
+    bash-completion
+    haxe-mode
+    proof-general
     ;; view
-    popwin image-dired+ image+ fill-column-indicator
-    hiwin highlight-current-line rainbow-mode
-    w3m swiper smartparens
+    popwin
+    image-dired+
+    image+
+    fill-column-indicator
+    hiwin
+    highlight-current-line
+    rainbow-mode
+    w3m
+    swiper
+    smartparens
     browse-kill-ring
     ; 起動時間測定
     esup
