aa875b79180518174e43358ca0febe5c996ca087
SAITOU Keita <keita44.f4@gmail.com>
2019-03-21 18:09:51 +0900

    Update: window-resizerをアップデートした
    Quitしたキーが無視されない！
    名前は自作関数としてmy-を採用。
    パッケージのやつを試したけど、Emacs標準同様操作感が気持ち悪いやつしかなかった。

--- .emacs.d/lisp/init_keybind.el
+++ .emacs.d/lisp/init_keybind.el
@@ -46,9 +46,10 @@
     (set-window-buffer thiswin nextbuf)))
 (global-set-key (kbd "C-x O") 'swap-screen)
 
-;;; 分割ウィンドウのサイズを変更するwindow-resizer
-; window-resizer関数
-(defun window-resizer ()
+;;; 分割ウィンドウのサイズを変更するmy-window-resizer
+;; my-window-resizer関数
+;; https://khiker.hatenablog.jp/entry/20100119/window_resize
+(defun my-window-resizer ()
   "Control window size and position."
   (interactive)
   (let ((window-obj (selected-window))
@@ -56,19 +57,27 @@
         (current-height (window-height))
         (dx (if (= (nth 0 (window-edges)) 0) 1 -1))
         (dy (if (= (nth 1 (window-edges)) 0) 1 -1))
-        c)
+        action c)
     (catch 'end-flag
       (while t
-        (message "size[%dx%d]" (window-width) (window-height))
-        (setq c (read-char))
+        (setq action
+              (read-key-sequence-vector
+               (format "size[%dx%d]" (window-width) (window-height))))
+        (setq c (aref action 0))
         (cond ((= c ?l) (enlarge-window-horizontally dx))
               ((= c ?h) (shrink-window-horizontally dx))
               ((= c ?j) (enlarge-window dy))
               ((= c ?k) (shrink-window dy))
               ;; otherwise
-              (t (message "Quit") (throw 'end-flag t)))))))
-; window-resizerをC-crにセット
-(global-set-key (kbd "C-c r") 'window-resizer)
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
+;; my-window-resizerをC-crにセット
+(global-set-key (kbd "C-c r") 'my-window-resizer)
 
 ;;; C-M-:に連番Evalを割り当て、ただしterminalならM-*
 (defun my-insert-repeat-numbers ()
