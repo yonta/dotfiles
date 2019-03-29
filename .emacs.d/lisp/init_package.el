30833bda9f3cd901c81f183af017db5e8c9ce2fc
SAITOU Keita <keita44.f4@gmail.com>
2019-03-29 17:01:30 +0900

    Refactor: use-packageのあれこれをリファクタした
    )が気に入らないので整えた。
    :initを正しく使うようにした。
    :initを見直した結果、:bindの設定も一部変更した。

--- .emacs.d/lisp/init_package.el
+++ .emacs.d/lisp/init_package.el
@@ -37,62 +37,52 @@
                ("<backtab>" . company-select-previous)
                ("C-f" . company-complete-selection)
                ("C-d" . company-show-doc-buffer)
-               ("C-s" . company-filter-candidates)
-               )))
+               ("C-s" . company-filter-candidates))))
 
 (use-package company-quickhelp
   :ensure t
-  :defer t
   :after company
-  :config
-  (company-quickhelp-mode t)
+  :init
+  (company-quickhelp-mode 1)
   :custom
-  (company-quickhelp-delay 1)
-  )
+  (company-quickhelp-delay 1))
 
 (use-package company-c-headers
   :ensure t
   :defer t
-  :after company
-  )
+  :after company)
 
 (use-package company-arduino
   :ensure t
   :defer t
-  :after company
-  )
+  :after company)
 
 ;; aptかpipでjediを入れておく
 ;; aptでpython3-jediをいれておき、初回起動時にjedi:install-serverする
 (use-package company-jedi
   :ensure t
   :defer t
-  :after (company jedi-core)
-  )
+  :after (company jedi-core))
 
 ;; aptかpipでflake8を入れておく
 ;; どちらを使うかを選択しないといけない、闇
 ;; aptでflake8をいれておく
 (use-package flycheck
-   :ensure t
-   :diminish flycheck-mode
-   :custom
-   (flycheck-python-flake8-executable "flake8")
-   :init
-   ;; 対応するメジャーモードでオート起動する
-   (global-flycheck-mode)
-   :config
-   ;; エラー箇所に背景色をつける
-   (set-face-background 'flycheck-error "pink")
-   :bind (:map flycheck-mode-map
-               ("M-p" . flycheck-previous-error)
-               ("M-n" . flycheck-next-error)
-               ))
+  :ensure t
+  :diminish flycheck-mode
+  :init
+  (global-flycheck-mode)
+  :custom
+  (flycheck-python-flake8-executable "flake8")
+  :config
+  (set-face-background 'flycheck-error "pink") ; エラー箇所は背景色をつける
+  :bind (:map flycheck-mode-map
+              ("M-p" . flycheck-previous-error)
+              ("M-n" . flycheck-next-error)))
 
 (use-package flycheck-ocaml
   :ensure t
-  :defer t
-  )
+  :defer t)
 
 (use-package cc-mode
   :defer t
@@ -107,9 +97,7 @@
     (set (make-local-variable 'company-backends)
          '((company-clang
             :with company-c-headers company-files company-dabbrev-code
-            company-yasnippet)))
-    (local-set-key (kbd "C-c C-c") #'quickrun)
-    )
+            company-yasnippet))))
   (add-hook 'c-mode-hook #'my-c-mode-hook)
   (defun my-c++-mode-hook ()
     "Setting for c++-mode."
@@ -122,42 +110,36 @@
     (set (make-local-variable 'company-backends)
          '((company-clang
             :with company-c-headers company-files company-dabbrev-code
-            company-yasnippet)))
-    (local-set-key (kbd "C-c C-c") #'quickrun)
-    )
-  (add-hook 'c++-mode-hook #'my-c++-mode-hook)
-  )
+            company-yasnippet))))
+  (add-hook 'c++-mode-hook #'my-c++-mode-hook))
 
 (use-package tuareg
   :ensure t
-  :defer t
-  )
+  :defer t)
 
 (use-package yasnippet
   :ensure t
   :diminish yas-minor-mode
   :after company
-  :config
+  :init
   (yas-global-mode 1)
-  (add-to-list 'company-backends #'company-yasnippet)
-  )
+  :config
+  (add-to-list 'company-backends #'company-yasnippet))
 
 ;; elpa/yasnippet-snippets-20190316.1019/snippets/ に以下をcloneしておく
 ;; https://github.com/jasperla/yasnippet-sml-mode
 ;; 使いづらいので、今後自作したい
 (use-package yasnippet-snippets
-  :ensure t
-  )
+  :ensure t)
 
+;; TODO: set compiler and libraries path by environment
 (use-package arduino-mode
   :ensure t
-  :defer t
-  ;; TODO: set compiler and libraries path by environment
-  )
+  :defer t)
 
 (use-package python-mode
   :ensure t
-  :mode "\\.py\\'"
+  :defer t
   :custom
   (indent-tabs-mode nil)
   (indent-level 4)
@@ -168,58 +150,51 @@
   (add-to-list 'company-backends
                '(company-jedi
                  :with company-files company-dabbrev-code company-yasnippet))
+  (define-key python-mode-map (kbd "C-c C-c") nil)
   :bind (:map python-mode-map
-              ("C-c C-c" . quickrun)
               ("C-c c" . py-execute-buffer)
-              ("<tab>" . indent-for-tab-command)
-              ))
+              ("<tab>" . indent-for-tab-command)))
 
 ;; aptかpipでautopep8を入れておく
 ;; aptでautopep8をいれておく
 (use-package py-autopep8
   :ensure t
   :if (executable-find "autopep8")
-  :hook (python-mode . py-autopep8-enable-on-save)
-  )
+  :hook (python-mode . py-autopep8-enable-on-save))
 
 (use-package quickrun
   :ensure t
-  :defer t
   :custom
   ;; タイムアウトで処理を中止させない
   (quickrun-timeout-seconds -1)
-  )
+  :bind (("C-c C-c" . quickrun)))
 
 ;; markdownコマンドをいれておく
 (use-package markdown-mode
   :ensure t
   :defer t
   :if (executable-find "markdown")
-  :init
+  :config
   ;; ファイルロック機能と競合してハングするため、leoさんの松葉杖対処を導入
   ;; https://groups.google.com/forum/#!topic/gnu.emacs.help/AIy5megeSHA
   (defun leo-markdown-fontify-buffer-wiki-links-empty ()
     "Empty replacement for `markdown-fontify-buffer-wiki-links` due to hanging bug."
     (interactive))
-  (eval-after-load "markdown-mode"
-    '(progn (fset #'markdown-fontify-buffer-wiki-links
-                  #'leo-markdown-fontify-buffer-wiki-links-empty)))
+  (fset #'markdown-fontify-buffer-wiki-links
+        #'leo-markdown-fontify-buffer-wiki-links-empty)
   :custom
   (markdown-command "markdown")
   ;; style sheetは生成HTMLと同フォルダにあるstyle.cssにする
-  (markdown-css-paths '("style.css"))
-  )
+  (markdown-css-paths '("style.css")))
 
 ;; markdownでコードブロックの編集のために必要
 (use-package edit-indirect
   :ensure t
-  :defer t
-  )
+  :defer t)
 
 (use-package csv-mode
   :ensure t
-  :defer t
-  )
+  :defer t)
 
 (use-package sml-mode
   :ensure t
@@ -238,21 +213,16 @@
                    '((company-mlton-keyword
                       company-mlton-basis
                       :with company-files company-dabbrev-code
-                      company-yasnippet)))))
-  )
+                      company-yasnippet))))))
 
 (use-package company-mlton
-  :config
-  (company-mlton-init)
-  )
+  :hook (sml-mode . company-mlton-init))
 
 ;; aptでgnupgを入れておく
 ;; alpaca.elが必要
 (use-package twittering-mode
   :ensure t
   :commands twit
-  ;; :init
-  ;; (setq twittering-suffix-space-size 8)
   :custom
   ;; use master passworad compressed by GnuPG
   (twittering-use-master-password t)
@@ -276,19 +246,13 @@
   (twittering-number-of-tweets-on-retrieval 100)
   (twittering-display-remaining t)
   (twittering-initial-timeline-spec-string
-   '(
-     "keita44_f4/friend"
-     ":replies"
-     ":home"
-     ))
-  ;;(twittering-tinyurl-service 'goo.gl)
+   '("keita44_f4/friend" ":replies" ":home"))
   ;;(twittering-retweet-format " RT @%s %t")
   (twittering-retweet-format " %u")
   (twittering-fill-column 80)
   (twittering-edit-skeleton 'inherit-mentions)
   :config
-  ;; use icon
-  (twittering-icon-mode t)
+  (twittering-icon-mode t) ; use icon
   ;; URLを青文字にする
   (add-hook 'twittering-mode-hook
             (lambda () (set-face-foreground 'twittering-uri-face "blue")))
@@ -301,35 +265,29 @@
               ("O" . twittering-push-uri-onto-kill-ring)
               ("J" . end-of-buffer)
               ("K" . twittering-goto-first-status)
-              ("u" . twittering-toggle-show-replied-statuses)
-              ))
+              ("u" . twittering-toggle-show-replied-statuses)))
 
 (use-package gnuplot-mode
   :ensure t
   ;; .gpl .plt、.gp .gnuplotはautoloadで登録済み
-  :mode ("\\.gpl\\'" "\\.plt\\'")
-  )
+  :mode ("\\.gpl\\'" "\\.plt\\'"))
 
 (use-package graphviz-dot-mode
   :ensure t
-  :defer t
-  )
+  :defer t)
 
 (use-package google-translate
   :ensure t
   :commands (google-translate google-translate-smooth-ui)
   :bind (("C-c C-t" . google-translate-smooth-translate)
-         ("C-c t" . google-translate-query-translate)
-         )
+         ("C-c t" . google-translate-query-translate))
   :config
   (setq google-translate-translation-directions-alist
-        '(("en" . "ja") ("ja" . "en")))
-  )
+        '(("en" . "ja") ("ja" . "en"))))
 
 (use-package bash-completion
   :ensure t
-  :commands shell
-  )
+  :commands shell)
 
 (use-package haxe-mode
   :ensure t
@@ -337,13 +295,11 @@
   :custom
   (tab-width 4)
   (indent-tabs-mode nil)
-  (fill-column 80)
-  )
+  (fill-column 80))
 
 (use-package proof-general
   :ensure t
-  :mode "\\.v\\'"
-  )
+  :mode "\\.v\\'")
 
 (use-package popwin
   :ensure t
@@ -351,18 +307,16 @@
   (popwin-mode 1)
   ;; popwin対象
   (setq popwin:special-display-config
-        '(
-          ("*quickrun*" :stick t)
+        '(("*quickrun*" :stick t)
           ("*Google Translate*")
           (completion-list-mode :noselect t) ;; 全completionを対象
           ("*Warnings*")
           (" *auto-async-byte-compile*")
-          ("*Kill Ring*")
-          )))
+          ("*Kill Ring*"))))
 
 ;; ImageMagickをaptでいれておく
+;; 非同期でimage-diredを動作させ、大量画像でフリーズしないようにするパッケージ
 (use-package image-dired+
-  ;; 非同期でimage-diredを動作させ、大量画像でフリーズしないようにする
   :ensure t
   :if (executable-find "convert")
   :commands image-dired
@@ -372,8 +326,7 @@
   :bind (:map image-dired-thumbnail-mode-map
               ("C-n" . image-diredx-next-line)
               ("C-p" . image-diredx-previous-line)
-              ("g" . revert-buffer) ; 更新
-              ))
+              ("g" . revert-buffer))) ; 更新
 
 (use-package image+
   :ensure t
@@ -385,56 +338,50 @@
   :bind (:map imagex-sticky-binding
               ("+" . imagex-sticky-zoom-in)
               ("-" . imagex-sticky-zoom-out)
-              ("0" . imagex-sticky-maximize)
-              ))
+              ("0" . imagex-sticky-maximize)))
 
+;; アクティブかどうかでバッファーのモードラインの色を変える
 (use-package hiwin
   :ensure t
   :init
   ;; (set-face-background 'hiwin-face "gray92")
-  ;; アクティブかどうかでバッファーのモードラインの色を変える
   (set-face-attribute 'mode-line nil :background "light sky blue")
   (set-face-attribute 'mode-line-inactive nil
                       :background "light gray"
-                      :foreground "dim gray")
-  )
+                      :foreground "dim gray"))
 
 (use-package highlight-current-line
   :ensure t
   :if window-system ; '-nw'で起動時は文字が見えなくなるのでオフにする
   :config
   (highlight-current-line-on t)
-  (set-face-background 'highlight-current-line-face "gray92")
-  )
+  (set-face-background 'highlight-current-line-face "gray92"))
 
+;; "#ff0000"などに色をつける
 (use-package rainbow-mode
   :ensure t
   :diminish rainbow-mode
-  :commands rainbow-mode
   :hook (c++-mode arduino-mode)
   :custom
   (rainbow-r-colors t)    ; R color listを使う
-  (rainbow-html-colors t) ; html color listを使う
-  )
+  (rainbow-html-colors t)) ; html color listを使う
 
 (use-package smartparens
   :ensure t
-  :defer t
   :diminish smartparens-mode
   :init
   (smartparens-global-mode t)
   ;; 一部のモードでは'での補完を行わない
   (sp-local-pair '(emacs-lisp-mode) "'" nil :actions nil)
   (sp-local-pair '(lisp-mode) "'" nil :actions nil)
+  (sp-local-pair '(lisp-mode) "`" nil :actions nil)
   (sp-local-pair '(sml-mode) "'" nil :actions nil)
   (sp-local-pair '(inferior-sml-mode) "'" nil :actions nil)
-  (sp-local-pair '(tuareg-mode) "'" nil :actions nil)
-  )
+  (sp-local-pair '(tuareg-mode) "'" nil :actions nil))
 
 (use-package browse-kill-ring
   :ensure t
-  :bind (("C-M-y" . browse-kill-ring))
-  )
+  :bind (("C-M-y" . browse-kill-ring)))
 
 ;; cmigemoをいれておく
 ;; https://github.com/koron/cmigemo
@@ -445,61 +392,51 @@
   (migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
   (migemo-user-dictionary nil)
   (migemo-regex-dictionary nil)
-  (migemo-init)
-  )
+  (migemo-init))
 
 (use-package avy-migemo
   :ensure t
-  :config
-  ;; 各modenoの設定ではなく、統合するここでmode onとbindする必要がある
-  ;; e.g.*.elのrequireは各設定にて行う必要がある
+  :after migemo
+  :init
   (avy-migemo-mode 1)
-  :bind (("M-x" . counsel-M-x)
-         ("M-r" . counsel-command-history)
-         ("C-x C-f" . counsel-find-file)
-         ("C-x b" . ivy-switch-buffer)
-         ("C-c C-d" . counsel-describe-function)
-         ("C-c C-g" . counsel-git-grep)
-         )
-  :bind (("C-s" . swiper)
-         ("C-c s" . isearch-forward)
-         )
-  :bind (:map swiper-map
-              ("M-%" . swiper-query-replace)
-              ("C-w" . ivy-yank-word)
-              ("C-M-y" . ivy-yank-char)
-              ))
+  (require 'avy-migemo-e.g.swiper)
+  (require 'avy-migemo-e.g.counsel))
 
 (use-package ivy
   :ensure t
-  :defer t
+  :after ivy-rich
   :custom
   (ivy-count-format "(%d/%d) ")
-  )
+  :bind ("C-x b" . ivy-switch-buffer))
 
 (use-package counsel
   :ensure t
-  :after ivy swiper
+  :after ivy swiper avy-migemo ivy-rich
   ;; dotファイルとコンパイルファイルなどを無視する
   ;; .キーを押せばdotスタートファイルは表示される
   :custom (counsel-find-file-ignore-regexp
            (concat "\\(\\`\\.\\)\\|"
                    (regexp-opt completion-ignored-extensions)))
   :config
-  (require 'avy-migemo-e.g.counsel)
-  :bind (:map counsel-find-file-map
-              ("^" . counsel-up-directory)
-              ))
+  :bind (("M-x" . counsel-M-x)
+         ("M-r" . counsel-command-history)
+         ("C-x C-f" . counsel-find-file)
+         ("C-c C-d" . counsel-describe-function)
+         ("C-c C-g" . counsel-git-grep)
+         (:map counsel-find-file-map
+               ("^" . counsel-up-directory))))
 
 (use-package swiper
   :ensure t
-  :after ivy
-  :defer t
+  :after ivy avy-migemo migemo
   :custom
   (swiper-include-line-number-in-search t)
-  :config
-  (require 'avy-migemo-e.g.swiper)
-  )
+  :bind (("C-s" . swiper)
+         ("C-c s" . isearch-forward)
+         (:map swiper-map
+               ("M-%" . swiper-query-replace)
+               ("C-w" . ivy-yank-word)
+               ("C-M-y" . ivy-yank-char))))
 
 ;; バグで動かない
 ;; https://github.com/jschaf/esup/issues/54
@@ -511,29 +448,28 @@
   (auto-package-update-delete-old-versions t)
   ;; (auto-package-update-hide-results t)
   :config
-  (auto-package-update-maybe)
-  )
+  (auto-package-update-maybe))
 
 (use-package auto-async-byte-compile
   :ensure t
-  :hook (emacs-lisp-mode . enable-auto-async-byte-compile-mode)
   ;; :custom
   ;; (auto-async-byte-compile-exclude-files-regexp "init*")
-  )
+  :hook (emacs-lisp-mode . enable-auto-async-byte-compile-mode))
 
 (use-package ivy-rich
   :ensure t
-  :custom
-  (ivy-format-function #'ivy-format-function-line)
-  :config
+  :defer t
+  :init
   (ivy-rich-mode 1)
-  )
+  :custom
+  (ivy-format-function #'ivy-format-function-line))
 
 (use-package git-gutter-fringe+
   :ensure t
   :diminish git-gutter+-mode
+  :init
+  (global-git-gutter+-mode 1)
   :config
-  (global-git-gutter+-mode)
   (set-face-foreground 'git-gutter+-added "lime green")
   (set-face-foreground 'git-gutter+-modified "blue")
   (fringe-helper-define 'git-gutter-fr+-modified nil
@@ -553,24 +489,22 @@
   "XXXXXXXX"
   "XXXXXXXX"
   "........"
-  "........")
-  )
+  "........"))
 
 (use-package dumb-jump
   :ensure t
+  :after ivy
   :bind (("M-g o" . dumb-jump-go-other-window)
          ("M-g j" . dumb-jump-go)
          ("M-g i" . dumb-jump-go-prompt))
   :config
-  (setq dumb-jump-selector 'ivy)
-  )
+  (setq dumb-jump-selector 'ivy))
 
 (use-package volatile-highlights
   :ensure t
   :diminish volatile-highlights-mode
   :config
-  (volatile-highlights-mode t)
-  )
+  (volatile-highlights-mode t))
 
 (use-package shell
   :init
@@ -584,11 +518,20 @@
   :hook (shell-mode . (lambda ()
                         ;; SHELL で ^M が付く場合は ^M を削除する
                         (set-buffer-process-coding-system
-                         'undecided-dos 'sjis-unix)))
-  )
+                         'undecided-dos 'sjis-unix))))
 
 (use-package dired
-  :init
+  :custom
+  ;; dired-modeがlsコマンドに渡すオプションを設定する
+  ;; l: 長い表示、dired-modeに必須のオプション
+  ;; g: ユーザ名を非表示
+  ;; G: グループ名を非表示
+  ;; h: kbyte・Mbyteの使用
+  ;; F: ディレクトリに「/」を表示
+  ;; A: 「.」と「..」を非表示でドットファイルを表示
+  ;;(setq dired-listing-switches "-gGhFA")
+  (dired-listing-switches "-lgGhF")
+  :config
   ;; サイズや拡張子による並び替えを追加する．
   ;; http://d.hatena.ne.jp/mooz/20091207/p1
   (defvar dired-various-sort-type
@@ -631,7 +574,6 @@
         (dired-sort-other
          (read-string "ls switches (must contain -l): " dired-actual-switches))
       (dired-various-sort-change dired-various-sort-type)))
-
   ;; diredでディレクトリを移動してもバッファを新規に作成しない
   (defun dired-my-advertised-find-file ()
     (interactive)
@@ -658,17 +600,6 @@ Creates a buffer if necessary."
                 (kill-buffer (current-buffer))
                 (dired up))
               (dired-goto-file dir))))))
-
-  ;; dired-modeがlsコマンドに渡すオプションを設定する
-  ;; l: 長い表示、dired-modeに必須のオプション
-  ;; g: ユーザ名を非表示
-  ;; G: グループ名を非表示
-  ;; h: kbyte・Mbyteの使用
-  ;; F: ディレクトリに「/」を表示
-  ;; A: 「.」と「..」を非表示でドットファイルを表示
-  ;;(setq dired-listing-switches "-gGhFA")
-  (setq dired-listing-switches "-lgGhF")
-
   ;; C-.でドットファイルの表示と非表示を切り替える
   (defun reload-current-dired-buffer ()
     "Reload current `dired-mode' buffer."
@@ -682,37 +613,31 @@ Creates a buffer if necessary."
       (if (string-match "[Aa]" dired-listing-switches)
           (setq dired-listing-switches "-lgGhF")
         (setq dired-listing-switches "-lgGhFA"))
-      (reload-current-dired-buffer))
-    )
-
+      (reload-current-dired-buffer)))
   :bind (:map dired-mode-map
               ("s" . dired-various-sort-change-or-edit)
               ("C-m" . dired-my-advertised-find-file)
               ("^" . dired-my-up-directory)
               ("C-." . toggle-dired-listing-switches)
-              ("r" . wdired-change-to-wdired-mode)
-              ))
+              ("r" . wdired-change-to-wdired-mode)))
 
 (use-package help-modehelp-mode
   ;; Alt+左右でヘルプの進む・戻るを行う、デフォルトはl/r
   :bind (:map help-mode-map
               ("M-<left>" . help-go-back)
-              ("M-<right>". help-go-forward)
-              ))
+              ("M-<right>". help-go-forward)))
 
 ;; コマンドラインと同じ色付けを使う
 (use-package ansi-color
   :commands ansi-color-for-comint-mode-on
-  :hook (shell-mode . ansi-color-for-comint-mode-on)
-  )
+  :hook (shell-mode . ansi-color-for-comint-mode-on))
 
 (use-package ibuf-ext
   :bind (("C-x C-b" . ibuffer)
          :map ibuffer-mode-map ("C-x f" . ibuffer-find-file))
   :config
   ;; ibuffer-find-fileを使わずにswiperを使う
-  (define-key ibuffer-mode-map (kbd "C-x C-f") nil)
-  )
+  (define-key ibuffer-mode-map (kbd "C-x C-f") nil))
 
 (use-package whitespace
   :diminish global-whitespace-mode
@@ -741,8 +666,7 @@ Creates a buffer if necessary."
   (defun set-whitespace-line-column-80 () (setq whitespace-line-column 80))
   (defun set-whitespace-line-column-100 () (setq whitespace-line-column 100))
   (add-hook 'java-mode-hook #'set-whitespace-line-column-100)
-  (add-hook 'change-major-mode-hook #'set-whitespace-line-column-80)
-  )
+  (add-hook 'change-major-mode-hook #'set-whitespace-line-column-80))
 
 (use-package elisp-mode
   :config
@@ -751,8 +675,7 @@ Creates a buffer if necessary."
               (set (make-local-variable 'company-backends)
                    '((company-capf
                       :with company-files company-dabbrev-code
-                      company-yasnippet)))))
-  )
+                      company-yasnippet))))))
 
 (use-package winner-mode
   :init
