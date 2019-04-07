251533fd99bf4f2012797d91cc0af1d14452e83e
SAITOU Keita <keita44.f4@gmail.com>
2019-04-07 15:19:08 +0900

    Refactor: load-path設定にオリジナル関数よりもEmacs定義の関数を使うようにした

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,19 +18,11 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;;; path
-;; load-pathを再帰的に追加する関数
-(defun add-to-load-path (&rest paths)
-(let (path)
-    (dolist (path paths paths)
-    (let ((default-directory (expand-file-name
-                              (concat user-emacs-directory path))))
-    (add-to-list 'load-path default-directory)
-     (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-         (normal-top-level-add-subdirs-to-load-path))))))
-
-;; 自分のカスタムemacs-lisp path
-(add-to-load-path "lisp")
+;;; 自分のカスタムemacs lispのpath
+;; 参考： https://www.emacswiki.org/emacs/LoadPath
+(let ((default-directory  "~/.emacs.d/lisp/"))
+  (normal-top-level-add-to-load-path '("."))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;;; 設定ファイルの読み込み
 (load "init_package")
