88aa6a042861595dfb09d6b028df404abfff3981
SAITOU Keita <keita44.f4@gmail.com>
2019-04-07 19:21:29 +0900

    Update: ibufferでキロメガ表示を使うようにした

--- .emacs.d/lisp/init_package.el
+++ .emacs.d/lisp/init_package.el
@@ -642,11 +642,25 @@ Creates a buffer if necessary."
   :hook (shell-mode . ansi-color-for-comint-mode-on))
 
 (use-package ibuf-ext
-  :bind (("C-x C-b" . ibuffer)
-         :map ibuffer-mode-map ("C-x f" . ibuffer-find-file))
   :config
-  ;; ibuffer-find-fileを使わずにswiperを使う
-  (define-key ibuffer-mode-map (kbd "C-x C-f") nil))
+  ;; キロメガでサイズ表示する
+  ;; 参考： https://www.emacswiki.org/emacs/IbufferMode
+  (define-ibuffer-column size-h
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 100000) (format "%7.0fk" (/ (buffer-size) 1000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size)))))
+  ;; Modify the default ibuffer-formats
+  (setq ibuffer-formats
+        '((mark modified read-only locked " " (name 18 18 :left :elide) " "
+                (size-h 9 -1 :right) " " (mode 16 16 :left :elide) " "
+                filename-and-process)))
+  ;; ibuffer-find-fileを使わずにcounselを使う
+  (define-key ibuffer-mode-map (kbd "C-x C-f") nil)
+  :bind (("C-x C-b" . ibuffer)
+         :map ibuffer-mode-map ("C-x f" . ibuffer-find-file)))
 
 (use-package whitespace
   :diminish global-whitespace-mode
