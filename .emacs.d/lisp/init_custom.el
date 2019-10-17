7493c96c78e85fe61830229a63cb9c668e4dedab
SAITOU Keita <keita44.f4@gmail.com>
2019-10-17 18:25:40 +0900

    Add: CCカスタム

--- .emacs.d/lisp/init_custom.el
+++ .emacs.d/lisp/init_custom.el
@@ -7,18 +7,31 @@
 
 ;;; ウィンドウについての設定
 (setq default-frame-alist
-      (append (list '(top . 0) ; 起動時の表示位置（上から）
-                    '(left . 0) ; 起動時の表示位置（左から）
-                    '(width . 194) ; 起動時のサイズ（幅）
-                    '(height . 47) ; 起動時のサイズ（縦）
-                    ;'(foreground-color . "#FFFFFF") ; 文字の色
-                    ;'(background-color . "gray30") ; 背景の色
-                    ;'(cursor-color . "gray") ; カーソルの色
-                    )
+      (append (list
+               '(top . 60) ; 起動時の表示位置（上から）
+               '(left . 5) ; 起動時の表示位置（左から）
+               '(width . 171) ; 起動時のサイズ（幅）
+               '(height . 38) ; 起動時のサイズ（縦）
+               ;;'(foreground-color . "#101010") ; 文字の色
+               ;;'(background-color . "#F0F0F0") ; 背景の色
+               ;;'(cursor-color . "gray") ; カーソルの色
+               )
               default-frame-alist))
 
 ;;; フォント設定
 (if window-system
     (add-to-list 'default-frame-alist '(font . "VL ゴシック-12" )))
+
+;;; flycheck-smlsharp
+(add-to-list 'load-path "~/git/flycheck-smlsharp/")
+(require 'flycheck-smlsharp)
+
+;;; ブラウザ設定
+;; aptでubuntu-wslをいれておく
+(eval-when-compile (require 'use-package))
+(use-package browse-url
+  :custom
+  (browse-url-browser-function 'browse-url-generic)
+  (browse-url-generic-program "wslview"))
 
 ;;; init_custom.el ends here
