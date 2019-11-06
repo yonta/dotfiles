c7b3832d795c6d55e06f73d459f34e5678469286
SAITOU Keita <keita44.f4@gmail.com>
2019-11-06 18:10:24 +0900

    Refactor: init_custom.elを編集しやすくした
    1. git diffに表示させない方法をコメントに書いた
    2. ウィンドウの位置について、改行位置を変更してdiffを取りやすくした

--- .emacs.d/lisp/init_custom.el
+++ .emacs.d/lisp/init_custom.el
@@ -1,20 +1,30 @@
 ;;; custom --- customization for each PC
 
 ;;; Commentary:
+;;
 ;; Customization for each PC to separate git diff.
+;;
+;; If you want that a changes of this file is not showen in `git diff',
+;; use below command.
+;;   git update-index --skip-worktree init_custom.el
+;; And, revert by below command.
+;;   git update-index --no-skip-worktree init_custom.el
+;; A reference is here
+;;   https://qiita.com/usamik26/items/56d0d3ba7a1300625f92
 
 ;;; Code:
 
 ;;; ウィンドウについての設定
 (setq default-frame-alist
-      (append (list '(top . 0) ; 起動時の表示位置（上から）
-                    '(left . 0) ; 起動時の表示位置（左から）
-                    '(width . 194) ; 起動時のサイズ（幅）
-                    '(height . 47) ; 起動時のサイズ（縦）
-                    ;'(foreground-color . "#FFFFFF") ; 文字の色
-                    ;'(background-color . "gray30") ; 背景の色
-                    ;'(cursor-color . "gray") ; カーソルの色
-                    )
+      (append (list
+               '(top . 0) ; 起動時の表示位置（上から）
+               '(left . 0) ; 起動時の表示位置（左から）
+               '(width . 194) ; 起動時のサイズ（幅）
+               '(height . 47) ; 起動時のサイズ（縦）
+               ;;'(foreground-color . "#FFFFFF") ; 文字の色
+               ;;'(background-color . "gray30") ; 背景の色
+               ;;'(cursor-color . "gray") ; カーソルの色
+               )
               default-frame-alist))
 
 ;;; フォント設定
