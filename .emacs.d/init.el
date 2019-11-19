4bf99a0d4cea135298d26a6ffa94c88e69764829
SAITOU Keita <keita44.f4@gmail.com>
2019-11-19 17:07:05 +0900

    Refactor: Emacsのカスタム変数を別ファイルに分離した

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,32 +42,8 @@
 (load "init_mode")
 (load "init_custom")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(avy-migemo-function-names
-   (quote
-    ((counsel-clj :around avy-migemo-disable-around)
-     (counsel-grep :around counsel-grep-migemo-around)
-     counsel-grep-function-migemo counsel-grep-occur-migemo
-     (counsel-git-occur :around counsel-git-occur-migemo-around)
-     (counsel-find-file-occur :around counsel-find-file-occur-migemo-around)
-     swiper--make-overlays-migemo
-     (swiper--re-builder :around swiper--re-builder-migemo-around)
-     (ivy--regex :around ivy--regex-migemo-around)
-     (ivy--regex-ignore-order :around ivy--regex-ignore-order-migemo-around)
-     (ivy--regex-plus :around ivy--regex-plus-migemo-around)
-     ivy--highlight-default-migemo ivy-occur-revert-buffer-migemo ivy-occur-press-migemo avy-migemo-goto-char avy-migemo-goto-char-2 avy-migemo-goto-char-in-line avy-migemo-goto-char-timer avy-migemo-goto-subword-1 avy-migemo-goto-word-1 avy-migemo-isearch avy-migemo-org-goto-heading-timer avy-migemo--overlay-at avy-migemo--overlay-at-full)))
- '(package-selected-packages
-   (quote
-    (groovy-mode company-prescient ivy-prescient yaml-mode dockerfile-mode lispxmp undo-tree rainbow-delimiters highlight-parentheses highlight-indentation auto-highlight-symbol paradox jedi-direx flycheck-mypy visual-regexp sudo-edit arduino-mode company flycheck ivy migemo yasnippet counsel swiper bind-key flycheck-popup-tip which-key recentf-ext async expand-region company-c-headers volatile-highlights dumb-jump git-gutter-fringe+ yasnippet-snippets use-package twittering-mode tuareg sml-mode smartparens rainbow-mode quickrun py-autopep8 proof-general popwin markdown-mode ivy-rich image-dired+ hiwin haxe-mode graphviz-dot-mode google-translate gnuplot-mode flycheck-ocaml edit-indirect diminish csv-mode company-quickhelp company-jedi company-arduino bash-completion auto-package-update auto-async-byte-compile))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; カスタム変数は別ファイルに保存する
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file) (load custom-file))
 
 ;;; init.el ends here
