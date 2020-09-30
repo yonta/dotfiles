7ea293caa7e9fbbfdee606b566e31c01000a5e70
SAITOU Keita <keita44.f4@gmail.com>
2020-09-30 16:29:55 +0900

    Fix: html/css以外でcompany候補がおかしい問題を修正
    company-bootstrapはmode判定が含まれないため、任意のモードで発火していた。
    そのため、emacs lispでcompany-dabbrev-codeしか使えない状態になっていた。
    emacs lispではcompany-capfが正しく発火するように、
    company-bootstrapは発火対象にならない:with以降にいれるようにした。

--- .emacs.d/lisp/init_package.el
+++ .emacs.d/lisp/init_package.el
@@ -488,16 +488,18 @@
     :after company
     :config
     (add-to-list 'company-backends
-                 '(company-web-html company-bootstrap
-                   :with company-dabbrev-code company-dabbrev)))
+                 '(company-web-html
+                   :with company-bootstrap
+                   company-dabbrev-code company-dabbrev)))
 
   (leaf css-mode
     :custom
     (css-indent-offset . 2)
     :config
     (add-to-list 'company-backends
-                 '(company-css company-bootstrap
-                               :with company-dabbrev-code company-dabbrev))))
+                 '(company-css
+                   :with  company-bootstrap
+                   company-dabbrev-code company-dabbrev))))
 
 (leaf javascript
   :init
