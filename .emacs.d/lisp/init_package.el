7e973759938d8ab312dfe2b6059bc9b443dc91d7
SAITOU Keita <keita44.f4@gmail.com>
2021-07-28 16:16:44 +0900

    Update: lsp-ui-docのdocへの移動をTABからC-c TABにした

--- .emacs.d/lisp/init_package.el
+++ .emacs.d/lisp/init_package.el
@@ -477,7 +477,7 @@
     ;; Emacsを--with-x-toolkit=gtk3 --with-xwidgetsでビルドする必要がある
     ;; (lsp-ui-doc-use-webkit . t)
     (lsp-ui-doc-delay . 0.7)
-    :bind (:lsp-ui-mode-map ("<tab>" . lsp-ui-doc-focus-frame)))
+    :bind (:lsp-ui-mode-map ("C-c <tab>" . lsp-ui-doc-focus-frame)))
   (leaf lsp-treemacs :ensure t
     :global-minor-mode lsp-treemacs-sync-mode)
   (leaf lsp-ivy :ensure t))
