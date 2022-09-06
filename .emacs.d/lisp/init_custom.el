ba7bd00f6c1363f83476c92e24da9b2150c71fd1
SAITOU Keita <keita44.f4@gmail.com>
2022-09-06 22:09:32 +0900

    Update: Ubuntu jammyでサイズを再設定

--- .emacs.d/lisp/init_custom.el
+++ .emacs.d/lisp/init_custom.el
@@ -17,12 +17,12 @@
 ;;; ウィンドウについての設定
 (setq default-frame-alist
       ;; 起動時の表示位置とサイズ。上から、左から、幅、高さの順
-      (append (list '(top . 55) '(left . 5) '(width . 252) '(height . 56))
+      (append (list '(top . 55) '(left . 5) '(width . 237) '(height . 62))
               default-frame-alist))
 
 ;;; フォント設定
 (if window-system
-    (add-to-list 'default-frame-alist '(font . "VL ゴシック-22")))
+    (add-to-list 'default-frame-alist '(font . "VL ゴシック-24")))
 
 ;;; カラーテーマの設定
 (setq custom-theme-directory "~/.emacs.d/themes")
