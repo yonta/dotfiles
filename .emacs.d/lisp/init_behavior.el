5dc6cfed8f73b1f6dd55f91a636a25cbe6f1c289
SAITOU Keita <keita44.f4@gmail.com>
2024-03-05 02:03:32 +0900

    Refactor: compile時計算化

--- .emacs.d/lisp/init_behavior.el
+++ .emacs.d/lisp/init_behavior.el
@@ -53,6 +53,6 @@
 
 ;;; Emacsのプロセス通信の容量を上げる
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/#increase-the-amount-of-data-which-emacs-reads-from-the-process
-(setq read-process-output-max (* 1024 1024))
+(setq read-process-output-max (eval-when-compile (* 1024 1024)))
 
 ;;; init_behavior.el ends here
