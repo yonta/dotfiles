20e290ce807271aeff7c0ca2e29ccd472d52e8d3
SAITOU Keita <keita44.f4@gmail.com>
2026-05-16 18:54:39 +0900

    Add: custom.elを保持してみる

---
+++ .config/emacs/custom.el
@@ -0,0 +1,55 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
+ '(native-comp-async-report-warnings-errors nil)
+ '(package-selected-packages
+   '(activities add-node-modules-path aggressive-indent async
+                auto-async-byte-compile auto-compile auto-highlight-symbol
+                biomejs-format browse-at-remote buffer-move cape cargo
+                colorful-mode company-bootstrap-icons company-bootstrap5
+                company-mlton company-web consult-better-jumper consult-eglot
+                copilot copilot-chat corfu csv-mode demap diff-hl diminish
+                dired-collapse dired-single dired-subtree direnv dirvish docker
+                docker-compose-mode dockerfile-mode dumb-jump edit-indirect
+                eglot-booster ellama elm-mode embark-consult erblint expreg
+                flycheck-color-mode-line flycheck-docker-build-checks
+                flycheck-eglot flycheck-markuplint flycheck-mlton flycheck-ocaml
+                flycheck-posframe flycheck-rust flycheck-smlsharp flyspell-popup
+                fontaine git-commit-ts-mode git-link git-modes git-timemachine
+                gnuplot-mode goggles google-translate gptel graphviz-dot-mode
+                grep-context grip-mode haxe-mode helpful highlight-indentation
+                highlight-parentheses hiwin hl-line+ hotfuzz htmlbeautifier
+                ialign idle-highlight-mode image-dired+ imenu-list
+                impatient-mode importmagic inf-ruby initchart jedi-core
+                jenkinsfile-mode keyfreq leaf-keywords lin lispxmp macrostep
+                marginalia migemo mmm-mode mozc-cand-posframe
+                nerd-icons-completion nerd-icons-corfu nerd-icons-dired
+                nerd-icons-ibuffer orderless origami pip-requirements popper
+                prettier-js py-isort pyvenv-auto quickrun rainbow-delimiters
+                rainbow-mode rbs-mode recentf-ext rg rspec-mode rubocop
+                rubocopfmt ruby-tools ruff-format rust-mode seeing-is-believing
+                show-font smart-jump smartparens sml-mode sudo-edit super-hint
+                systemd tabnine tempel-collection terraform-mode treemacs
+                treesit-auto ts-comint typescript-mode vc-msg vertico
+                visual-regexp vundo wakatime-mode web-mode which-key yard-mode))
+ '(savehist-additional-variables '(kill-ring))
+ '(warning-suppress-log-types '((comp) (copilot) (nativecomp)))
+ '(warning-suppress-types '((comp)))
+ '(wakatime-api-key "waka_XXXX")
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-hl-change ((t (:background "#fad987"))) nil "Customized with leaf in `diff-hl' block")
+ '(diff-hl-delete ((t (:background "#fa8d87"))) nil "Customized with leaf in `diff-hl' block")
+ '(diff-hl-insert ((t (:background "#95fa87"))) nil "Customized with leaf in `diff-hl' block")
+ '(dired-collapse-shadow ((t (:inherit dired-directory))) nil "Customized with leaf in `dired-collapse' block at `/tmp/emacs-async-comp-init_package-ivehvC.el'")
+ '(tab-line ((t (:foreground "black" :background "gray90"))) nil "Customized with leaf in `tab-line-mode' block")
+ '(tab-line-tab-current ((t (:inherit highlight))) nil "Customized with leaf in `tab-line-mode' block")
+ '(tab-line-tab-inactive ((t (:background "gray84"))) nil "Customized with leaf in `tab-line-mode' block")
+ '(tab-line-tab-modified ((t (:foreground "red"))) nil "Customized with leaf in `tab-line-mode' block"))
